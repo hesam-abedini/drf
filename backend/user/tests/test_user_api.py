8d7f7430650781e583f5e339da57b7b172ab4d62
hesam <hesamz60@gmail.com>
2024-01-15 15:19:16 +0330

    added authentication

--- backend/user/tests/test_user_api.py
+++ backend/user/tests/test_user_api.py
@@ -10,6 +10,8 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL=reverse('user:create')
+TOKEN_URL=reverse('user:token')
+ME_URL=reverse('user:me')
 
 def create_user(**params):
     """create and return a new user"""
@@ -61,3 +63,91 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
         user_exists=get_user_model().objects.filter(email=payload['email']).exists()
         self.assertFalse(user_exists)
+
+    def create_token_for_user(self):
+
+        user_details={
+            'name':'hesam',
+            'email':'test@example.com',
+            'password':'testpass213'
+        }
+        create_user(**user_details)
+
+        payload={
+            'email':user_details['email'],
+            'paasword':user_details['password']
+        }
+
+        res=self.client.post(TOKEN_URL,payload)
+
+        self.assertIn('token',res.data)
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
+
+    def create_token_for_user_bad_cred(self):
+
+        user_details={
+            'name':'hesam',
+            'email':'test@example.com',
+            'password':'testpass213'
+        }
+        create_user(**user_details)
+
+        payload={
+            'email':'ali@gmail.com',
+            'paasword':user_details['password']
+        }
+
+        res=self.client.post(TOKEN_URL,payload)
+
+        self.assertNotIn('token',res.data)
+        self.assertEqual(res.status_code,status.HTTP_400_BAD_REQUEST)
+
+    def test_retrive_user_unathorized(self):
+
+        res=self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code,status.HTTP_401_UNAUTHORIZED)
+
+class PrivateUserAppiTest(TestCase):
+
+    def setUp(self):
+        
+        self.user=create_user(
+            email='test@test.com',
+            password='testpass123',
+            name='ali test'
+        )
+        self.client=APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_rerive_profile_success(self):
+        
+        res=self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
+        self.assertEqual(res.data,{
+            'name':self.user.name,
+            'email':self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+
+        res=self.client.post(ME_URL,{})
+
+        self.assertEqual(res.status_code,status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        
+        payload={
+            'name':'updated name',
+            'password':'newpassword123'
+        }
+
+        res=self.client.patch(ME_URL,payload)
+
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.name,payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
+
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics,authentication,permissions
+from rest_framework.authtoken.views import ObtainAuthToken
 
-# Create your views here.
+from user.serializers import UserSerializer,AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    
+    serializer_class=UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class=AuthTokenSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class=UserSerializer
+    authentication_classes=[authentication.TokenAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
